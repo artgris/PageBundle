92d6b6ec3803fc2a9fc58a692fb1b0ce7e1db186
artgris <a.gribet@gmail.com>
2019-06-04 17:58:51 +0200

    add tests

---
+++ tests/RestrictionTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Artgris\Bundle\PageBundle\Tests;
+
+use Artgris\Bundle\PageBundle\Tests\Fixtures\AbstractTestCase;
+
+class RestrictionTest extends AbstractTestCase
+{
+    protected static $options = ['environment' => 'redirection'];
+
+    public function testEditManager()
+    {
+        $this->requestEditView();
+        $this->assertSame(
+            200,
+            static::$client->getResponse()->getStatusCode()
+        );
+    }
+
+}
