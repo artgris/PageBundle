e92b0898cfa2a2ff16ef86d62616f0a457fc3843
artgris <a.gribet@gmail.com>
2019-06-23 14:02:58 +0200

    fix - isEmpty function

--- src/Entity/ArtgrisBlockTranslation.php
+++ src/Entity/ArtgrisBlockTranslation.php
@@ -26,4 +26,26 @@ class ArtgrisBlockTranslation
     {
         $this->contentTranslatable = $contentTranslatable;
     }
+
+    public function isEmpty()
+    {
+
+        foreach (get_object_vars($this) as $var => $value) {
+            if (in_array($var, ['id', 'translatable', 'locale'])) {
+                continue;
+            }
+
+            if (!empty($value)) {
+                if (is_array($value)) {
+                    if (!empty(array_filter($value))) {
+                    return false;
+                    }
+                } else {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    
 }
