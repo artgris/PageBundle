a71ef16eaaed3a7ab704261e0f4f1dbce8321602
artgris <a.gribet@gmail.com>
2020-02-09 20:05:22 +0100

    fix 4.3 => 4.4

--- src/Repository/ArtgrisBlockRepository.php
+++ src/Repository/ArtgrisBlockRepository.php
@@ -4,7 +4,7 @@ namespace Artgris\Bundle\PageBundle\Repository;
 
 use Artgris\Bundle\PageBundle\Entity\ArtgrisBlock;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Symfony\Bridge\Doctrine\RegistryInterface;
+use Doctrine\Common\Persistence\ManagerRegistry;
 
 /**
  * @method ArtgrisBlock|null find($id, $lockMode = null, $lockVersion = null)
@@ -14,7 +14,7 @@ use Symfony\Bridge\Doctrine\RegistryInterface;
  */
 class ArtgrisBlockRepository extends ServiceEntityRepository
 {
-    public function __construct(RegistryInterface $registry)
+    public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, ArtgrisBlock::class);
     }
--- src/Repository/ArtgrisPageRepository.php
+++ src/Repository/ArtgrisPageRepository.php
@@ -4,7 +4,7 @@ namespace Artgris\Bundle\PageBundle\Repository;
 
 use Artgris\Bundle\PageBundle\Entity\ArtgrisPage;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Symfony\Bridge\Doctrine\RegistryInterface;
+use Doctrine\Common\Persistence\ManagerRegistry;
 
 /**
  * @method ArtgrisPage|null find($id, $lockMode = null, $lockVersion = null)
@@ -14,7 +14,7 @@ use Symfony\Bridge\Doctrine\RegistryInterface;
  */
 class ArtgrisPageRepository extends ServiceEntityRepository
 {
-    public function __construct(RegistryInterface $registry)
+    public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, ArtgrisPage::class);
     }
