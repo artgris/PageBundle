81ec2234366442c0e5d8b1dcd449ab50e9adaf98
artgris <a.gribet@gmail.com>
2022-07-11 11:27:53 +0200

    optimisation

--- src/Twig/BlockExtension.php
+++ src/Twig/BlockExtension.php
@@ -67,7 +67,7 @@ class BlockExtension extends AbstractExtension
         $block = $this->getBlocks();
 
         // get from cache
-        if (isset($block[$bloc])) {
+        if (array_key_exists($bloc, $block)) {
             return $block[$bloc];
         }
 
